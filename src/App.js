7f372423d019eced4f5b5da25c49cdfdc0bfb999
manikantang <75244279+manikantang@users.noreply.github.com>
2021-12-26 00:27:53 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <CreateArea />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
